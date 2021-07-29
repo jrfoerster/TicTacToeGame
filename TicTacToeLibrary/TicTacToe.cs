189ed779caf5cee053b02cea62ad4bd2b2fe0543
Justin Foerster <jrfoerster@gmail.com>
2021-07-28 21:46:03 -0400

    attempt to un-hardcode TicTacToe Check methods

--- TicTacToeLibrary/TicTacToe.cs
+++ TicTacToeLibrary/TicTacToe.cs
@@ -85,30 +85,80 @@ namespace TicTacToeLibrary
                 || Check(2, 4, 6);
         }
 
-        //private bool Check(int a, int b, int c)
+        //private bool CheckRows()
         //{
-        //    return Check(_squares[a], _squares[b], _squares[c]);
+        //    int length = (int)Math.Sqrt(_squares.Length);
+        //    bool output = false;
+        //    for (int row = 0; row < length; row++)
+        //    {
+        //        var indexes = new int[length];
+        //        for (int col = 0; col < length; col++)
+        //        {
+        //            int index = row * length + col;
+        //            Console.WriteLine(index);
+        //            indexes[col] = index;                 
+        //        }
+        //        output |= Check(indexes);
+        //    }
+        //    return output;
         //}
 
-        //private bool Check(Square one, Square two, Square three)
+        //private bool CheckColumns()
         //{
-        //    return (one != Square.Empty) && (one == two) && (one == three);
+        //    int length = (int)Math.Sqrt(_squares.Length);
+        //    bool output = false;
+        //    for (int row = 0; row < length; row++)
+        //    {
+        //        var indexes = new int[length];
+        //        for (int col = 0; col < length; col++)
+        //        {
+        //            int index = col * length + row;
+        //            Console.WriteLine(index);
+        //            indexes[col] = index;              
+        //        }
+        //        output |= Check(indexes);
+        //    }
+        //    return output;
         //}
 
-        private bool Check(params int[] indexes)
+        private bool Check(int a, int b, int c)
         {
-            int firstIndex = indexes[0];
-            Square first = _squares[firstIndex];
-            bool output = first != Square.Empty;
-            if (output)
-            {
-                for (int i = 1; i < indexes.Length; i++)
-                {
-                    int index = indexes[i];
-                    output &= (first == _squares[index]);
-                }
-            }
-            return output;
+            return Check(_squares[a], _squares[b], _squares[c]);
         }
+
+        private bool Check(Square one, Square two, Square three)
+        {
+            return (one != Square.Empty) && (one == two) && (one == three);
+        }
+
+        //private bool Check(params int[] indexes)
+        //{
+        //    int firstIndex = indexes[0];
+        //    Square first = _squares[firstIndex];
+        //    bool output = first != Square.Empty;
+        //    if (output)
+        //    {
+        //        for (int i = 1; i < indexes.Length; i++)
+        //        {
+        //            int index = indexes[i];
+        //            output &= first == _squares[index];
+        //        }
+        //    }
+        //    return output;
+        //}
+
+        //private bool Check(params Square[] squares)
+        //{
+        //    Square one = squares[0];
+        //    bool output = one != Square.Empty;
+        //    if (output)
+        //    {
+        //        for (int i = 1; i < squares.Length; i++)
+        //        {
+        //            output &= one == squares[i];
+        //        }
+        //    }
+        //    return output;
+        //}
     }
 }
