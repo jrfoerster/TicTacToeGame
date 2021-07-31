47c10f72a429ee8c64e17c31b3aeaf9896f686a5
Justin Foerster <jrfoerster@gmail.com>
2021-07-31 17:35:50 -0400

    extracted WinCondition class for TicTacToe.IsWon

--- TicTacToeLibrary/TicTacToe.cs
+++ TicTacToeLibrary/TicTacToe.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 
 namespace TicTacToeLibrary
 {
@@ -17,7 +15,7 @@ namespace TicTacToeLibrary
         private int _moveCount = 0;
 
         public IReadOnlyList<Square> Squares => _squares;
-        public bool IsWon => CheckRows() || CheckColumns() || CheckDiagonals();
+        public bool IsWon => WinCondition.Check(_squares);
         public bool HasNoMoreMoves => _moveCount == _squares.Length;
 
         public TicTacToe()
@@ -53,101 +51,5 @@ namespace TicTacToeLibrary
         {
             return square != Square.Empty;
         }
-
-        private bool CheckRows()
-        {
-            return Check(0, 1, 2)
-                || Check(3, 4, 5)
-                || Check(6, 7, 8);
-        }
-
-        private bool CheckColumns()
-        {
-            return Check(0, 3, 6)
-                || Check(1, 4, 7)
-                || Check(2, 5, 8);
-        }
-
-        private bool CheckDiagonals()
-        {
-            return Check(0, 4, 8)
-                || Check(2, 4, 6);
-        }
-
-        private bool Check(int a, int b, int c)
-        {
-            return Check(_squares[a], _squares[b], _squares[c]);
-        }
-
-        private bool Check(Square one, Square two, Square three)
-        {
-            return (one != Square.Empty) && (one == two) && (one == three);
-        }
-
-        //private bool CheckRows()
-        //{
-        //    int length = (int)Math.Sqrt(_squares.Length);
-        //    bool output = false;
-        //    for (int row = 0; row < length; row++)
-        //    {
-        //        var indexes = new int[length];
-        //        for (int col = 0; col < length; col++)
-        //        {
-        //            int index = row * length + col;
-        //            Console.WriteLine(index);
-        //            indexes[col] = index;                 
-        //        }
-        //        output |= Check(indexes);
-        //    }
-        //    return output;
-        //}
-
-        //private bool CheckColumns()
-        //{
-        //    int length = (int)Math.Sqrt(_squares.Length);
-        //    bool output = false;
-        //    for (int row = 0; row < length; row++)
-        //    {
-        //        var indexes = new int[length];
-        //        for (int col = 0; col < length; col++)
-        //        {
-        //            int index = col * length + row;
-        //            Console.WriteLine(index);
-        //            indexes[col] = index;              
-        //        }
-        //        output |= Check(indexes);
-        //    }
-        //    return output;
-        //}
-
-        //private bool Check(params int[] indexes)
-        //{
-        //    int firstIndex = indexes[0];
-        //    Square first = _squares[firstIndex];
-        //    bool output = first != Square.Empty;
-        //    if (output)
-        //    {
-        //        for (int i = 1; i < indexes.Length; i++)
-        //        {
-        //            int index = indexes[i];
-        //            output &= first == _squares[index];
-        //        }
-        //    }
-        //    return output;
-        //}
-
-        //private bool Check(params Square[] squares)
-        //{
-        //    Square one = squares[0];
-        //    bool output = one != Square.Empty;
-        //    if (output)
-        //    {
-        //        for (int i = 1; i < squares.Length; i++)
-        //        {
-        //            output &= one == squares[i];
-        //        }
-        //    }
-        //    return output;
-        //}
     }
 }
---
+++ TicTacToeLibrary/WinCondition.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+
+namespace TicTacToeLibrary
+{
+    internal class WinCondition
+    {
+        private static IReadOnlyList<Square> _squares;
+
+        public static bool Check(IReadOnlyList<Square> squares)
+        {
+            _squares = squares;
+            return CheckRows() || CheckColumns() || CheckDiagonals();
+        }
+
+        private static bool CheckRows()
+        {
+            return Check(0, 1, 2)
+                || Check(3, 4, 5)
+                || Check(6, 7, 8);
+        }
+
+        private static bool CheckColumns()
+        {
+            return Check(0, 3, 6)
+                || Check(1, 4, 7)
+                || Check(2, 5, 8);
+        }
+
+        private static bool CheckDiagonals()
+        {
+            return Check(0, 4, 8)
+                || Check(2, 4, 6);
+        }
+
+        private static bool Check(int a, int b, int c)
+        {
+            return Check(_squares[a], _squares[b], _squares[c]);
+        }
+
+        private static bool Check(Square one, Square two, Square three)
+        {
+            return (one != Square.Empty) && (one == two) && (one == three);
+        }
+
+        //private bool CheckRows()
+        //{
+        //    int length = (int)Math.Sqrt(_squares.Length);
+        //    bool output = false;
+        //    for (int row = 0; row < length; row++)
+        //    {
+        //        var indexes = new int[length];
+        //        for (int col = 0; col < length; col++)
+        //        {
+        //            int index = row * length + col;
+        //            Console.WriteLine(index);
+        //            indexes[col] = index;                 
+        //        }
+        //        output |= Check(indexes);
+        //    }
+        //    return output;
+        //}
+
+        //private bool CheckColumns()
+        //{
+        //    int length = (int)Math.Sqrt(_squares.Length);
+        //    bool output = false;
+        //    for (int row = 0; row < length; row++)
+        //    {
+        //        var indexes = new int[length];
+        //        for (int col = 0; col < length; col++)
+        //        {
+        //            int index = col * length + row;
+        //            Console.WriteLine(index);
+        //            indexes[col] = index;              
+        //        }
+        //        output |= Check(indexes);
+        //    }
+        //    return output;
+        //}
+
+        //private bool Check(params int[] indexes)
+        //{
+        //    int firstIndex = indexes[0];
+        //    Square first = _squares[firstIndex];
+        //    bool output = first != Square.Empty;
+        //    if (output)
+        //    {
+        //        for (int i = 1; i < indexes.Length; i++)
+        //        {
+        //            int index = indexes[i];
+        //            output &= first == _squares[index];
+        //        }
+        //    }
+        //    return output;
+        //}
+
+        //private bool Check(params Square[] squares)
+        //{
+        //    Square one = squares[0];
+        //    bool output = one != Square.Empty;
+        //    if (output)
+        //    {
+        //        for (int i = 1; i < squares.Length; i++)
+        //        {
+        //            output &= one == squares[i];
+        //        }
+        //    }
+        //    return output;
+        //}
+    }
+}
