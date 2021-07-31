34e14fe4644ff665dcda0ee87352cad651cd0d0c
Justin Foerster <jrfoerster@gmail.com>
2021-07-31 16:37:58 -0400

    added Game class to represent a single game of TicTacToe

---
+++ TicTacToeLibrary/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeLibrary
+{
+    public class Game
+    {
+        private readonly TicTacToe _board = new TicTacToe();
+        private bool _isPlayerX = true;
+
+        public IReadOnlyList<Square> Squares => _board.Squares;
+        public Square PlayerSquare => _isPlayerX ? Square.X : Square.O;
+        public bool IsActive { get; private set; } = true;
+        public bool IsWon { get; private set; } = false;
+        public bool HasNoMoreMoves { get; private set; } = false;
+
+        public bool ChangeSquare(int index)
+        {
+            bool wasChanged = _board.ChangeSquare(index, PlayerSquare);
+            if (wasChanged)
+            {
+                IsWon = _board.IsWon;
+                HasNoMoreMoves = _board.HasNoMoreMoves;
+                IsActive = (IsWon == false) && (HasNoMoreMoves == false);
+
+                if (IsWon == false)
+                {
+                    _isPlayerX = !_isPlayerX;
+                }
+            }
+            return wasChanged;
+        }
+    }
+}
